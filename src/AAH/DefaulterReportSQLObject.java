e33e9b6899c2a6b75247d28a9973e671d29603d4
Bhavya Garg <bhavya@gatech.edu>
2014-12-02 22:52:09 -0500

    FINAL QUERY FIX FOR DEFAULT REPORT QUERY

--- src/AAH/DefaulterReportSQLObject.java
+++ src/AAH/DefaulterReportSQLObject.java
@@ -16,9 +16,16 @@ public class DefaulterReportSQLObject {
 
         String defaulterReportStatement = "SELECT LeaseReport.Apt_Number, (LeaseReport.Amount - LeaseReport.Rent), (DAY(LeaseReport.Date_Of_Payment) - 3)\n" +
                 "FROM (SELECT * FROM APARTMENT Apt JOIN PAYS_RENT PR ON Apt.Apt_Number = PR.Apartment_Number) AS LeaseReport\n" +
-                "WHERE (YEAR(LeaseReport.Date_Of_Payment) = YEAR(CURDATE())) AND \n" +
-                "(MONTH(LeaseReport.Date_Of_Payment) = " +  month_req + ") AND\n" +
-                "(DAY(LeaseReport.Date_Of_Payment) > 3);";//PUT SQL CODE HERE
+                "                WHERE (YEAR(LeaseReport.Date_Of_Payment) = YEAR(CURDATE())) AND\n" +
+                "                (MONTH(LeaseReport.Date_Of_Payment) =" +  month_req + ") AND\n" +
+                "                (DAY(LeaseReport.Date_Of_Payment) > 3) AND\n" +
+                "                LeaseReport.Apt_Number NOT IN(\n" +
+                "\t                SELECT A.Apt_Number FROM APARTMENT A NATURAL JOIN RESIDENT R NATURAL JOIN PROSPECTIVE_RESIDENT PRes\n" +
+                "\t                WHERE (YEAR(LeaseReport.Date_Of_Payment) = YEAR(PRes.Preferred_Move_In_Date)) AND\n" +
+                "               \t\t(MONTH(LeaseReport.Date_Of_Payment) = MONTH(PRes.Preferred_Move_In_Date)) AND\n" +
+                "               \t\t(DAY(PRes.Preferred_Move_In_Date) > 7)\n" +
+                "               \t\t);";
+
         System.out.println("query: " + defaulterReportStatement);
         try {
             ResultSet rs = SQLConnector.runQuery(defaulterReportStatement);
